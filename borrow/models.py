effceb565b79f2b0421b2da4d549fd879f8f97cd
Ubuntu <sakuk@ubuntu-server-01.g45vwhqlxvxefkkyivcj3ir0og.fx.internal.cloudapp.net>
2023-04-13 13:42:39 +0000

    created template base for better scalability

--- borrow/models.py
+++ borrow/models.py
@@ -2,19 +2,22 @@ from django.db import models
 import datetime
 
 # Create your models here.
+
+
 class Product(models.Model):
     # NON NULLABLE values - must be filled
     name = models.CharField(max_length=50)
     genre = models.CharField(max_length=30)
-    descrtiption = models.CharField(max_length=250)
-    thumbnail_path = models.CharField(max_length=50)
     amount = models.SmallIntegerField(default=0)
+    descrtiption = models.CharField(max_length=250)
     release_date = models.DateField(default=datetime.date.today)
     loaned_amount = models.SmallIntegerField(default=0)
     # author = models.ManyToManyField(author)
     # NULLABLE fields - can be left empty
     genre = models.CharField(max_length=30, blank=True, null=True)
     language = models.CharField(max_length=30, blank=True, null=True)
+    # we could use a randomly chosen thumbnail for if there is no image provided
+    thumbnail_path = models.CharField(max_length=50)
     # category
     # purchase_event
 
--- lainakoodarit/settings.py
+++ lainakoodarit/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY") or ""
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["20.123.65.125"]
 
 
 # Application definition
