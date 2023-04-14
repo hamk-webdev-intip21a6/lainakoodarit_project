933c8a97efd98d4ab2eaaf30f01f78b89abdbdee
waisatifi <atifiwais@hotmail.com>
2023-04-14 18:05:27 +0300

    fixed some bugs

--- borrow/admin.py
+++ borrow/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from .models import Product
+from .models import Product, Author, Event, User
 
 admin.site.register(Product)
-=======
-from .models import Product, Author, Event, User
->>>>>>> 564bc0de712cccea564e80dc9aa56230ae227e48
+
+
