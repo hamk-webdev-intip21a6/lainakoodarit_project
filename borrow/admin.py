4db46de9a79332dc6c63c2cc402f03ffa37c5df5
sakuexe <saku.karttunen@gmail.com>
2023-04-15 13:39:09 +0300

    changed the admin interface for products

--- borrow/admin.py
+++ borrow/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 from .models import Product, Author, Event, User
 
-admin.site.register(Product)
+
+class AdminProduct(admin.ModelAdmin):
+    list_display = ('name', 'short_description', 'genre',
+                    'date_added', 'loaned_amount')
+    search_fields = ('description', 'name')
+    list_filter = ('date_added', 'loaned_amount')
+
+    def short_description(self, obj):
+        """Create a shorter version of a description for the
+        Admin interface"""
+        CHAR_LIMIT = 60
+        if len(obj.description) > CHAR_LIMIT:
+            return (obj.description[:CHAR_LIMIT] + '...')
+        return obj.description
+
+    short_description.short_description = 'Description'
+
+
+admin.site.register(Product, AdminProduct)
 
 admin.site.register(Author)
 
