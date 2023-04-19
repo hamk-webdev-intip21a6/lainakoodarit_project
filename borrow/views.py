f1cbb4781435589b3500a7197e931299b2cb52f9
sakuexe <saku.karttunen@gmail.com>
2023-04-19 15:32:17 +0300

    fixes to randomization of images

--- borrow/views.py
+++ borrow/views.py
@@ -1,21 +1,25 @@
 # Create your views here.
 from django.views import generic
 from .models import Product
+from random import randint
 
 
 def random_default_thumbnail(context, is_list: bool = True):
     """Get a random image for a product, if product doesn't have one already.
     @param context, is_list"""
+    # randomize a number from 1 to 5
     # if context for DetailView is wanted
     if not is_list:
+        random_num = randint(1, 5)
         if not context['object'].image:
-            context['object'].image = f"defaults/placeholder-{context['object'].category}-1.jpg"
+            context['object'].image = f"defaults/placeholder-{context['object'].category}-{random_num}.jpg"
         return context
 
     # if ListView is wanted
     for obj in context['object_list']:
+        random_num = randint(1, 5)
         if not obj.image:
-            obj.image = f"defaults/placeholder-{obj.category}-1.jpg"
+            obj.image = f"defaults/placeholder-{obj.category}-{random_num}.jpg"
     return context
 
 
@@ -28,7 +32,6 @@ class IndexView(generic.ListView):
         # get the context
         context = super().get_context_data(**kwargs)
         context = random_default_thumbnail(context)
-
         return context
 
     def get_queryset(self):
@@ -61,6 +64,12 @@ class ProductListView(generic.ListView):
     template_name = 'borrow/product_list.html'
     context_object_name = 'listings'
     model = Product
+
+    def get_context_data(self, **kwargs):
+        # get the context
+        context = super().get_context_data(**kwargs)
+        context = random_default_thumbnail(context)
+        return context
 
     def get_queryset(self):
         """Check if the template passed a GET query.
