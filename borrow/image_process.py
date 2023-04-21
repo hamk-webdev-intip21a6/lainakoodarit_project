d41dacd20e4df68ee2d4a69433d5d393fcb117eb
Ubuntu <sakuk@ubuntu-server-01.g45vwhqlxvxefkkyivcj3ir0og.fx.internal.cloudapp.net>
2023-04-21 09:52:04 +0000

    fixed thumbnail generation and implimentation for index.html

--- borrow/image_process.py
+++ borrow/image_process.py
@@ -4,12 +4,19 @@ import os
 
 
 def save_thumbnail(file_path, file_name) -> str:
+    """ save a copy of a given image file and save it to thumbnails folder
+    returns the usable path to the thumbnail"""
+    thumbnail_folder = os.path.join(settings.MEDIA_ROOT, 'thumbnails')
+    # check if thumbnails folder exists, if not, then create one
+    if not os.path.exists(thumbnail_folder):
+        os.makedirs(thumbnail_folder)
+
     with Image.open(file_path) as img:
-        img.thumbnail((400, 400))
+        img.thumbnail((400, 400), Image.ANTIALIAS)
         # thumb_path = os.path.splitext(file_path)[0] + '_thumb.webp'
         filename = os.path.splitext(os.path.basename(file_name))[0]
         copy_filename = f'{filename}.webp'
         copy_path = os.path.join(
             settings.MEDIA_ROOT, 'thumbnails', copy_filename)
         img.save(copy_path, format="webp")
-    return os.path.join('media', 'thumbnails', copy_filename)
+    return os.path.join('/media', 'thumbnails', copy_filename)
