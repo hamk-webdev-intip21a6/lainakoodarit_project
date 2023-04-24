ef9284ce84b5ec63fec8c13f68f3cd8078f30265
sakuexe <saku.karttunen@gmail.com>
2023-04-24 12:10:22 +0300

    removed the random thumbnails and images, changed to default ones based on category

--- borrow/views.py
+++ borrow/views.py
@@ -4,26 +4,10 @@ from .models import Product
 from random import randint
 
 
-def random_default_thumbnail(category: str) -> str:
-    """Get a random image for a product, if product doesn't have one already.
-    @param context, is_list"""
-    # if context for DetailView is wanted
-    random_num = randint(1, 5)
-    return f"defaults/placeholder-{category}-{random_num}.jpg"
-
-
 class IndexView(generic.ListView):
     template_name = 'borrow/index.html'
     # what name to use in the template for the data returned in get_queryset
     context_object_name = 'listings'
-
-    def get_context_data(self, **kwargs):
-        # get the context
-        context = super().get_context_data(**kwargs)
-        for obj in context['object_list']:
-            if not obj.image:
-                obj.image = random_default_thumbnail(obj.category)
-        return context
 
     def get_queryset(self):
         """ Return the five most recent objects in the product category """
@@ -40,9 +24,6 @@ class ProductView(generic.DetailView):
         """Calculate the amount of copies available and add to the view context"""
         context = super().get_context_data(**kwargs)
         # get a default image for product, if image does not exist
-        if not context['object'].image:
-            context['object'].image = random_default_thumbnail(
-                context['object'].category)
         product = self.get_object()
         # calculate the amount of available copies
         available = product.amount - product.loaned_amount
@@ -53,9 +34,6 @@ class ProductView(generic.DetailView):
         current_category = context['object'].category
         context['recent_additions'] = Product.objects.filter(
             category=current_category)[:5]
-        for obj in context['recent_additions']:
-            if not obj.image:
-                obj.image = random_default_thumbnail(obj.category)
         return context
 
 
@@ -63,14 +41,6 @@ class ProductListView(generic.ListView):
     template_name = 'borrow/product_list.html'
     context_object_name = 'listings'
     model = Product
-
-    def get_context_data(self, **kwargs):
-        # get the context
-        context = super().get_context_data(**kwargs)
-        for obj in context['object_list']:
-            if not obj.image:
-                obj.image = random_default_thumbnail(obj.category)
-        return context
 
     def get_queryset(self):
         """Check if the template passed a GET query.
