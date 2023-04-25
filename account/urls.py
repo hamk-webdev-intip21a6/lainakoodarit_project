aeba51065e6d096154e464ebd34594c38dc4442c
AleksiHel <97727800+AleksiHel@users.noreply.github.com>
2023-04-25 18:48:05 +0300

    Added return to profile page. Needs testing

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import SignUpView, UserProfileView
+from .views import SignUpView, UserProfileView, ReturnLoanView
 
 app_name = 'account'
 urlpatterns = [
+    path('return_loan/', ReturnLoanView.as_view(), name='return_loan'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', SignUpView.as_view(), name='signup'),
--- account/views.py
+++ account/views.py
@@ -1,12 +1,12 @@
 # Create your views here.
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.views import generic
+from django.views import generic, View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView
-from borrow.models import UserProfile, Event
-from datetime import datetime, timedelta
-
+from borrow.models import UserProfile, Event, Product
+from django.utils import timezone
+from django.shortcuts import redirect, reverse
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
@@ -24,21 +24,24 @@ class UserProfileView(LoginRequiredMixin, DetailView):
         return self.request.user.userprofile
 
 
-    # spagetit alempaan, jos muutetaan models 
-    #   def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['user_loans'] = Event.objects.filter(user=self.request.user)
-        
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        user_loans = Event.objects.filter(user=self.request.user)
+        context['user_loans'] = Event.objects.filter(user=self.request.user)
 
-        user_loans_with_return_dates = []
-        for loan in user_loans:
-            return_date = loan.loaned_date + timedelta(days=14)
-            user_loans_with_return_dates.append((loan, return_date))
-
-        context['user_loans_with_return_dates'] = user_loans_with_return_dates
         return context
-        
+    
+class ReturnLoanView(LoginRequiredMixin, View):
+
+    def post(self, request):
+        loan_id = request.POST.get('loan_id')
+        loan = Event.objects.get(id=loan_id, user=request.user)
+        loan.return_date = timezone.now()
+        loan.save()
+
+        product = Product.objects.get(id=loan.product.id)
+        product.loaned_amount -= 1
+        product.save()
+
+        redirect_url = reverse('account:profile')
+        return redirect(f"{redirect_url}?success=true&event=return")
     
