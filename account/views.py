c8f097b2596086db14d7bed340039ae9aec22d91
AleksiHel <97727800+AleksiHel@users.noreply.github.com>
2023-04-25 15:05:58 +0300

    Loans linked to profile page

--- account/views.py
+++ account/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render
-
 # Create your views here.
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView
-from borrow.models import UserProfile
+from borrow.models import UserProfile, Event
+from datetime import datetime, timedelta
 
 
 class SignUpView(generic.CreateView):
@@ -19,5 +18,27 @@ class UserProfileView(LoginRequiredMixin, DetailView):
     template_name = 'profile/user_profile.html'
     context_object_name = 'profile'
 
+
+
     def get_object(self, queryset=None):
         return self.request.user.userprofile
+
+
+    # spagetit alempaan, jos muutetaan models 
+    #   def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['user_loans'] = Event.objects.filter(user=self.request.user)
+        
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user_loans = Event.objects.filter(user=self.request.user)
+
+        user_loans_with_return_dates = []
+        for loan in user_loans:
+            return_date = loan.loaned_date + timedelta(days=14)
+            user_loans_with_return_dates.append((loan, return_date))
+
+        context['user_loans_with_return_dates'] = user_loans_with_return_dates
+        return context
+        
+    
