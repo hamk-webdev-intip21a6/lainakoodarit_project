3e029ea1a4ec0e56da94fed4f36a10d455b378fc
sakuexe <saku.karttunen@gmail.com>
2023-04-27 16:41:04 +0300

    adjusted look and location of the signup and login views and templates

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import SignUpView, UserProfileView, ReturnLoanView
+from .views import SignUpView, UserProfileView, ReturnLoanView, LoggingInView
 
 app_name = 'account'
 urlpatterns = [
     path('return_loan/', ReturnLoanView.as_view(), name='return_loan'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/', LoggingInView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', SignUpView.as_view(), name='signup'),
     path('profile/', UserProfileView.as_view(), name='profile'),
--- account/views.py
+++ account/views.py
@@ -1,8 +1,9 @@
 # Create your views here.
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic, View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
 from django.views.generic import DetailView
 from borrow.models import UserProfile, Event, Product
 from django.utils import timezone
@@ -10,28 +11,32 @@ from django.shortcuts import redirect, reverse
 from django.db.models import ObjectDoesNotExist
 
 
+class LoggingInView(LoginView):
+    form_class = AuthenticationForm
+    template_name = 'account/login.html'
+
+
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('account:login')
+    template_name = 'account/signup.html'
+
 
 class UserProfileView(LoginRequiredMixin, DetailView):
     model = UserProfile
     template_name = 'profile/user_profile.html'
     context_object_name = 'profile'
 
-
-
     def get_object(self, queryset=None):
         return self.request.user.userprofile
-
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['user_loans'] = Event.objects.filter(user=self.request.user)
 
         return context
-    
+
+
 class ReturnLoanView(LoginRequiredMixin, View):
 
     def post(self, request):
@@ -44,9 +49,8 @@ class ReturnLoanView(LoginRequiredMixin, View):
             loan.save()
         except ObjectDoesNotExist:
             return redirect(redirect_url + '?success=false&event=return')
-        
+
         product = Product.objects.get(id=loan.product.id)
         product.loaned_amount -= 1
-        product.save()       
+        product.save()
         return redirect(f"{redirect_url}?success=true&event=return")
-    
