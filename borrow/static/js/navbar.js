872bac8e30b4f9cf7dc4ea478abd9f69512008b5
sakuexe <saku.karttunen@gmail.com>
2023-04-27 14:19:47 +0300

    added navbar animation and create account link to index

--- borrow/static/js/navbar.js
+++ borrow/static/js/navbar.js
@@ -7,12 +7,30 @@ const navButtonIcons = {
   close: "/static/icons/navbutton-close.svg",
 };
 
+// await function for adding delay
+function delay(ms = 200) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 navButton.addEventListener("click", (event) => {
   // toggle navigation bar button's state
   isNavOpen = !isNavOpen;
   console.log(isNavOpen);
   console.log(navButtonIcon.src);
   // change the navbutton's icon based on the isNavOpen variable
-  navButtonIcon.src = navButtonIcons[isNavOpen ? "close" : "open"];
-  navList.classList.toggle("hidden");
+  navButtonIcon.src = navButtonIcons[isNavOpen ? "open" : "close"];
+  // navList.classList.toggle("hidden");
+  toggleNavigation();
 });
+
+async function toggleNavigation() {
+  if (!isNavOpen) {
+    navList.classList.toggle("hidden");
+    await delay();
+    navList.classList.toggle("opacity-0");
+    return;
+  }
+  navList.classList.toggle("opacity-0");
+  await delay();
+  navList.classList.toggle("hidden");
+}
