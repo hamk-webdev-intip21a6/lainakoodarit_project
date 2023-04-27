0d2737e7602c64f15903220f86ff3388aa59497a
sakuexe <saku.karttunen@gmail.com>
2023-04-27 15:42:28 +0300

    visual changes and added filter buttons to the product-list.html template

---
+++ borrow/static/js/alert.js
@@ -0,0 +1,26 @@
+const alertBox = document.querySelector("section.alert");
+const cancelAlertButton = document.querySelector(".alert>div>button");
+
+// await function for adding delay
+function delay(ms = 200) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+cancelAlertButton.addEventListener("click", () => {
+  removeAlertButton();
+});
+
+fadeAndMove();
+
+async function removeAlertButton() {
+  fadeAndMove();
+  await delay(500);
+  // remove from visible/interactable DOM
+  alertBox.classList.add("hidden");
+}
+
+function fadeAndMove() {
+  // looks like the alert box moves up and fades away
+  alertBox.classList.toggle("-translate-y-6");
+  alertBox.classList.toggle("opacity-0");
+}
