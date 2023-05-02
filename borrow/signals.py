ea9fec1b851a7e52c4c3a628aedf8bc808916c3c
sakuexe <saku.karttunen@gmail.com>
2023-05-02 09:12:56 +0300

    created signals for removing unused image files of products

--- borrow/signals.py
+++ borrow/signals.py
@@ -1,15 +1,48 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.fields.files import default_storage
+from django.db.models.signals import pre_delete, pre_save
 from django.dispatch import receiver
+from django.conf import settings
+import os
 from .models import Product
 
 
-# @receiver(post_save, sender=Product)
-# def on_save(sender, instance, created, **kwargs) -> None:
-#     """Only activate this signal when Product's image propery is saved"""
-#     if not created:
-#         return
+@receiver(pre_save, sender=Product)
+def on_save(sender, instance, **kwargs) -> None:
+    """If new Product.image is different from the previous
+    remove the old product images"""
+    if not instance.pk and not instance.image:
+        return
+    # get the old object and check if the product already exists
+    old_object = Product.objects.filter(pk=instance.pk).first()
+    if not old_object:
+        return
+    old_image_path = old_object.image.path
+    # check if the image path has been changed
+    if old_image_path == instance.image.path:
+        return
+    try:
+        image_name = os.path.splitext(os.path.basename(old_image_path))[0]
+        thumbnail_path = os.path.join(
+            settings.MEDIA_ROOT, 'thumbnails', f"{image_name}.webp")
+        # remove the old thumbnail
+        os.remove(thumbnail_path)
+        # remove the old full image
+        os.remove(old_image_path)
+    except OSError as error:
+        print('OSERROR:', error)
 
-@receiver(post_delete, sender=Product)
+
+@receiver(pre_delete, sender=Product)
 def on_delete(sender, instance, **kwargs) -> None:
-    """Delete the files that we're used for the deleted image"""
+    """Delete the thumbnail and image of a file when product is removed"""
+    try:
+        image_name = os.path.splitext(os.path.basename(instance.image.path))[0]
+        thumbnail_path = os.path.join(
+            settings.MEDIA_ROOT, 'thumbnails', f"{image_name}.webp")
+        # remove the thumbnail
+        os.remove(thumbnail_path)
+        # and the full image
+        os.remove(instance.image.path)
+    except:
+        pass
     return
