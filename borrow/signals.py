447140bdf26b16d8d69934072e28a1123634ee04
sakuexe <saku.karttunen@gmail.com>
2023-05-02 11:11:43 +0300

    adjusted the signals functions to be more readable and maintainable

--- borrow/signals.py
+++ borrow/signals.py
@@ -1,4 +1,3 @@
-from django.db.models.fields.files import default_storage
 from django.db.models.signals import pre_delete, pre_save
 from django.dispatch import receiver
 from django.conf import settings
@@ -6,43 +5,42 @@ import os
 from .models import Product
 
 
-@receiver(pre_save, sender=Product)
-def on_save(sender, instance, **kwargs) -> None:
-    """If new Product.image is different from the previous
-    remove the old product images"""
-    if not instance.pk and not instance.image:
-        return
-    # get the old object and check if the product already exists
-    old_object = Product.objects.filter(pk=instance.pk).first()
-    if not old_object:
-        return
-    old_image_path = old_object.image.path
-    # check if the image path has been changed
-    if old_image_path == instance.image.path:
-        return
+def remove_images(image_path: str) -> None:
+    """Remove the old product images"""
     try:
-        image_name = os.path.splitext(os.path.basename(old_image_path))[0]
-        thumbnail_path = os.path.join(
-            settings.MEDIA_ROOT, 'thumbnails', f"{image_name}.webp")
-        # remove the old thumbnail
-        os.remove(thumbnail_path)
-        # remove the old full image
-        os.remove(old_image_path)
-    except OSError as error:
-        print('OSERROR:', error)
-
-
-@receiver(pre_delete, sender=Product)
-def on_delete(sender, instance, **kwargs) -> None:
-    """Delete the thumbnail and image of a file when product is removed"""
-    try:
-        image_name = os.path.splitext(os.path.basename(instance.image.path))[0]
+        image_name = os.path.splitext(os.path.basename(image_path))[0]
         thumbnail_path = os.path.join(
             settings.MEDIA_ROOT, 'thumbnails', f"{image_name}.webp")
         # remove the thumbnail
         os.remove(thumbnail_path)
         # and the full image
-        os.remove(instance.image.path)
-    except:
-        pass
+        os.remove(image_path)
+    except OSError as error:
+        print('OSError:', error)
     return
+
+
+@receiver(pre_save, sender=Product)
+def on_save(sender, instance, **kwargs) -> None:
+    """If new Product.image is different from the previous
+    remove the old product images"""
+    try:
+        # get the old object and it's path
+        old_object = Product.objects.filter(pk=instance.pk).first()
+        old_image_path = old_object.image.path
+        # check that the instance's image has changed
+        instance.image.path
+    except OSError as error:
+        return print("OSError:", error)
+    except:
+        return
+    # check if the image path has been changed
+    if old_image_path == instance.image.path:
+        return
+    remove_images(old_image_path)
+
+
+@receiver(pre_delete, sender=Product)
+def on_delete(sender, instance, **kwargs) -> None:
+    """Delete the thumbnail and image of a file when product is removed"""
+    remove_images(instance)
